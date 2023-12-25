485afab87eaa66e927427d3ba2902960dc85ddd0
IsaacLL <isaaclealga@gmail.com>
2023-12-25 16:20:30 -0400

    commit inicial

---
+++ Ke/Contador.py
@@ -0,0 +1,20 @@
+# Primera prueba
+
+#c = Contador de numeros
+#n = Numero a solicitar
+
+print("Prueba de contador")
+
+c=0
+n=int(input("Dime un numero de contador -  \n"))
+
+while c<n:
+    c+=1
+    print(c, end=", ")
+
+    if c == n:
+        print("Finalizado")
+        break
+
+
+##Finalizado
---
+++ Ke/Untitled-1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Holaola mundo" << std::endl;
+    return 0;
+}
